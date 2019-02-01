8543541a3c006ef609afa7689c5d58c5373efdf4
apple <apple@apples-MacBook-Pro.local>
2019-02-01 17:00:40 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("You flipped a " + cardsInPlay[0]);
+console.log("You flipped a " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+	
+	if(cardsInPlay[0]===cardsInPlay[1]) {
+
+		alert("You found a match");
+	}else{
+
+		alert("Sorry try again");
+	}
+
+}
+
+
