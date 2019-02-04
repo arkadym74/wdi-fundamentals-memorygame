399de9062a99eb7c339b647ecc866b5228369255
apple <apple@apples-MacBook-Pro.local>
2019-02-04 17:07:15 -0500

    create gamelogic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,38 +1,66 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
 var cards = [
  {
  	rank: "queen",
  	suit: "hearts",
- 	cardImage: "images/queen-of-hearts.png"
+ 	cardImage: 'images/queen-of-hearts.png'
  },
 
  {
  	rank: "queen",
  	suit: "diamonds",
- 	cardImage: "images/queen-of-diamonds.png"
+ 	cardImage: 'images/queen-of-diamonds.png'
  },
 
  {
  	rank: "king",
  	suit: "hearts",
- 	cardImage: "images/king-of-hearts.png"
+ 	cardImage: 'images/king-of-hearts.png'
  },
 
  {
  	rank: "king",
  	suit: "diamonds",
- 	cardImage: "images/king-of-diamonds.png"
+ 	cardImage: 'images/king-of-diamonds.png'
  }
 
 
 ];
+
 var cardsInPlay = [];
-//var cardId;
+
+var checkForMatch = function() {
+
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		
+		alert("You found a match");
+	  }else {
+		alert("Sorry try again");
+		}
+
+  }
+
+
+var flipCard = function() {
+
+var cardId = this.getAttribute('data-id');
+
+this.setAttribute('src',cards[cardId].cardImage);
+
+cardsInPlay.push(cards[cardId].rank);
+
+if (cardsInPlay.length === 2) {
+
+	checkForMatch();
+  }
+};
 
 var createBoard = function(){
 	for(var i = 0; i < cards.length; i++) {
 		
 		var cardElement = document.createElement('img');
-		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('src','images/back.png');
 		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener('click', flipCard)
 		document.getElementById('game-board').appendChild(cardElement);
@@ -40,37 +68,6 @@ var createBoard = function(){
 	}
 };
 
-var flipCard = function() {
-
-var cardId = this.getAttribute('data-id');
-
-this.setAttribute('src',cards[cardId].cardImage);
-   
-	if(cardsInPlay[cardId]===cardsInPlay[cardId]) {
-
-		alert("You found a match");
-	
-	}else{
-
-		alert("Sorry try again");
-	}
-
-//checkForMatch();
-
-};
-
-/*var checkForMatch = function() {
-
-	this.setAttribute("src", cards[cardId].cardImage);
-   
-	if(cardsInPlay[0]===cardsInPlay[1]) {
-
-		alert("You found a match");
-	}else{
-
-		alert("Sorry try again");
-	}
-};*/
-
 createBoard();
 
+});
